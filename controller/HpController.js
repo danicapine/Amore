145ece755e3cc7d366217882f98f15ff88a037e0
Danica <danicacolobong1@gmail.com>
2024-09-30 23:05:19 +0800

    ulit, ayaw gumana ng links

--- controller/HpController.js
+++ controller/HpController.js
@@ -2,5 +2,8 @@ const con = {
     index:(req, res)=>{
         res.render('index');   
     },
+    product:(req, res)=>{
+        res.render('product');   
+    },
 };
 module.exports = con;
--- routes/router.js
+++ routes/router.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const con = require('../controller/HpController'); 
 router.get('/', con.index);
+router.get('/', con.product);
 module.exports = router;
